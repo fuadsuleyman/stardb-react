aa92088134c7bb36cc67b7590d2491cbe3827123
fuad_suleymanov <fuadsuleymanov@outlook.com>
2020-04-26 16:00:38 +0400

    Updates

---
+++ precache-manifest.713034be5ddb7f2db0da2d794cd7da0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf80dce5e3a6318daf4069c0dd7ce4d7",
+    "url": "/fuadsuleyman/stardb-react/index.html"
+  },
+  {
+    "revision": "3cca1cd33689b5443549",
+    "url": "/fuadsuleyman/stardb-react/static/css/main.29b7f63d.chunk.css"
+  },
+  {
+    "revision": "11bc67e1acc3078ccaee",
+    "url": "/fuadsuleyman/stardb-react/static/js/2.44404c4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fuadsuleyman/stardb-react/static/js/2.44404c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cca1cd33689b5443549",
+    "url": "/fuadsuleyman/stardb-react/static/js/main.2d204e29.chunk.js"
+  },
+  {
+    "revision": "035b760808964fec4b62",
+    "url": "/fuadsuleyman/stardb-react/static/js/runtime-main.3d69770e.js"
+  },
+  {
+    "revision": "aa1e2de23749cbbf3253d126bcf5c1ad",
+    "url": "/fuadsuleyman/stardb-react/static/media/death-star.aa1e2de2.png"
+  }
+]);
